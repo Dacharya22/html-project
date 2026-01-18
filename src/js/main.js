011eaa4302f24b2c5dac78a51d0ffe76f3f928d3
Dacharya22 <darshnaacharya22@gmail.com>
2026-01-18 22:58:38 +0530

    change code

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,29 @@ btn.addEventListener('click', table);
 
 function table() {
     let inp = document.getElementById('inp');
-    let num = Number(inp.value);
-    inp.value = "";
+    
+    
+    // // let inp = document.getElementById('inp');
+    // // let num = Number(inp.value);
+    // // inp.value = "";
 
-    let ul = document.getElementById("ou-ul");
-    ul.innerHTML = ""; // clear old table
+    // // let ul = document.getElementById("ou-ul");
+    // // ul.innerHTML = ""; // clear old table
 
-    let tableColor = (num % 2 === 0) ? "green" : "red";
+    // // let tableColor = (num % 2 === 0) ? "green" : "red";
 
-    let i = 1;
-    while (i <= 10) {
-        let tab = num * i;
+    // // let i = 1;
+    // // while (i <= 10) {
+    // //     let tab = num * i;
 
-        let list = document.createElement('li');
-        list.textContent = num + " * " + i + " = " + tab;
+    // //     let list = document.createElement('li');
+    // //     list.textContent = num + " * " + i + " = " + tab;
 
-        list.style.color = tableColor;
+    // //     list.style.color = tableColor;
 
-        ul.appendChild(list);
-        i++;
-    }
+    // //     ul.appendChild(list);
+    // //     i++;
+    // }
 }
 
 let btn2 = document.getElementById('btn-2');
